7cca3d8d98c461295df8665f726c2273aba8c5b2
Samuel Mitas <samuel.mitas@devinsolutions.com>
2019-03-05 15:21:48 +0100

    Use configuration file

---
+++ config/configuration.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	logging "github.com/op/go-logging"
+	"github.com/spf13/viper"
+)
+
+var logger = logging.MustGetLogger("config")
+
+// Config is a configuration structure
+type Config struct {
+	VNS          VNS
+	Construction Construction
+	Optimization Optimization
+}
+
+type Construction struct {
+	Strategy string
+	LevelMax int
+}
+
+type VNS struct {
+	IterMax int
+}
+
+type Optimization struct {
+	Strategy string
+	IterMax  int
+	LevelMax int
+}
+
+// LoadConfig loads configuration file
+func (c *Config) LoadConfig(conf string) (err error) {
+	viper.SetConfigFile(conf)
+
+	if err = viper.ReadInConfig(); err != nil {
+		logger.Errorf("Couldn't load file %s\n", conf)
+		return
+	}
+
+	if err = viper.UnmarshalKey("vns", &c.VNS); err != nil {
+		logger.Error(err.Error())
+		return
+	}
+
+	if err = viper.UnmarshalKey("construction", &c.Construction); err != nil {
+		logger.Error(err.Error())
+		return
+	}
+
+	if err = viper.UnmarshalKey("optimization", &c.Optimization); err != nil {
+		logger.Error(err.Error())
+		return
+	}
+
+	logger.Info("Configuration loaded successfully")
+	return
+}
